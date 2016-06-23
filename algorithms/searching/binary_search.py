8bd292fe24cfa49c9c981b2ff3fc18aa34a05c24
Justin McGuire <jm@landedstar.com>
2016-06-23 14:12:44 -0400

    binary search

---
+++ algorithms/searching/binary_search.py
@@ -0,0 +1,45 @@
+# binary search
+#
+# divide and conquer
+# best time:    O(1)
+# average time: O(logn)
+# worst time:   O(logn)
+#
+# required a list of sorted elements
+# look at the half-way mark, compare to e, move up or down, repeat
+
+def binary_search(A, e, start=0, end=None, prev_middle=None):
+  if end is None:
+    end = len(A)
+
+  middle = (start + end) // 2
+  check = A[middle]
+  
+  #print "    start %d, end %d, middle %d (%s), prev_middle %s" % (start, end, middle, str(check), str(prev_middle))
+
+  # check for failure
+  if start > end or middle == prev_middle:
+    return False
+
+  if e == check:
+    return middle
+  elif e > check:
+    return binary_search(A, e, middle, end, middle)
+  elif e < check:
+    return binary_search(A, e, start, middle, middle)
+
+# testing
+
+def test(A, e):
+  print "looking for", e
+  position = binary_search(A, e)
+  if position is not False:
+    print " -> found item at", position
+  else:
+    print " -> did not find item"
+
+A = range(1,100)
+
+for e in range (0,101):
+  test(A, e)
+
