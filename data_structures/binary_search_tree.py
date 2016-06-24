b9c673baefbcb4d5fdbea63c4dbbc6fa6bae0148
Justin McGuire <jm@landedstar.com>
2016-06-24 16:04:49 -0400

    added deleting to binary_search_tree
     - also added parent to node, to make things a notch easier
     - added some todo items to binary_search_tree
     - need a proper testing scafold

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -4,6 +4,7 @@ class BinaryNode(object):
     self.e = e
     self.left = None
     self.right = None
+    self.parent = None
 
   def add(self, e):
     """add the new element in the appropriate place"""
@@ -12,15 +13,29 @@ class BinaryNode(object):
         self.left.add(e)
       else:
         node = BinaryNode(e)
+        node.parent = self
         self.left = node
     else:
       if self.right is not None:
         self.right.add(e)
       else:
         node = BinaryNode(e)
+        node.parent = self
         self.right = node
 
-class BinaryTree(object):
+  def find_furthest_right(self):
+    if self.right:
+      return self.right.find_furthest_right()
+    else:
+      return self
+
+  def find_furthest_left(self):
+    if self.left:
+      return self.left.find_furthest_left()
+    else:
+      return self
+
+class BinarySearchTree(object):
   """mostly just holds the root node of a binary tree"""
   def __init__(self):
     self.root = None
@@ -33,17 +48,102 @@ class BinaryTree(object):
       self.root.add(new)
 
   def __contains__(self, e):
-    """does the tree contain the value e"""
+    """a expressive alias for find()"""
+    if self.find(e):
+      return True
+    else:
+      return False
+
+  def find(self, e):
+    """return the node with e if it exists"""
     current = self.root
 
     while current is not None:
       if e == current.e:
-        return True
+        return current
       elif e < current.e:
         current = current.left
       elif e > current.e:
         current = current.right
+
     return False
+
+  def rm(self, e):
+    """remove the node with e, if it exists"""
+
+    """ how to remove a node in a binary tree:
+     we need three nodes:
+      - parent: to_delete's parent
+      - advance: to_delete's most rightmost node on it's left side (with no left children)
+      - advance_parent: that node's parent
+
+     if we're in the middle of a complicated tree, then we'll make three moves:
+      1. advance takes the place of to_delete
+      2. advance's right child is moved to advance_parent.left
+      3. to_delete's children are moved to advance
+    """
+
+    to_delete = self.find(e)
+    if not to_delete:
+      raise Exception("error: can't remove, doesn't exist")
+    parent = to_delete.parent
+
+    # if there are no children, this will be easy
+    if to_delete.left is None and to_delete.right is None:
+      if parent is None: # it's a root node
+        self.root = None
+      elif parent.left == to_delete:
+        parent.left = None
+      elif parent.right == to_delete:
+        parent.right = None
+      else:
+        raise Exception("parent doesn't think it's related to to_delete")
+      return
+
+    advance = None
+    advance_parent = None
+
+    # 2. find advance, and handle its child
+    if to_delete.left is not None:
+      advance = to_delete.left.find_furthest_right()
+
+      advance_parent = advance.parent
+      advance_parent.right = advance.left
+
+      if advance.right:
+        advance.right.parent = advance_parent.left
+
+    elif to_delete.right is not None:
+      advance = to_delete.right.find_furthest_left()
+
+      advance_parent = advance.parent
+      advance_parent.left = advance.right
+
+      if advance.left:
+        advance.left.parent = advance_parent.right
+
+    else:
+      raise Exception("to_delete suddenly has no children, but it did a minute ago")
+
+    # 1. move advance to delete's spot
+    advance.parent = to_delete.parent
+    if parent is None:
+      # we're a root node
+      self.root = advance
+    elif parent.left == to_delete:
+      to_delete.parent.left = advance
+    elif parent.right == to_delete:
+      to_delete.parent.right = advance
+    else:
+      raise Exception("something has gone wrong")
+
+    # 3. handle to_delete's children
+    advance.left = to_delete.left
+    advance.right = to_delete.right
+    if to_delete.left:
+      to_delete.left.parent = advance
+    if to_delete.right:
+      to_delete.right.parent = advance
 
   def show_all(self):
     """show all nodes, breadth-first"""
@@ -53,6 +153,8 @@ class BinaryTree(object):
     while nodes:
       node = nodes.pop(0) # basically, shift.
       print "node:", node.e,
+      if node.parent:
+        print "(parent: %s)" % str(node.parent.e),
       if node.left:
         print "- left: ", node.left.e,
         nodes.append(node.left)
@@ -62,7 +164,7 @@ class BinaryTree(object):
       print
 
   def show_all_visual(self, depth=0, node=None):
-    """show all nodes, nicely formatted"""
+    """show all nodes, nicely formatted, depth-first"""
     indent = "  " * depth
     if node is None:
       node = self.root
@@ -73,3 +175,21 @@ class BinaryTree(object):
       self.show_all_visual(depth+1, node.left)
     if node.right:
       self.show_all_visual(depth+1, node.right)
+
+  # TODO
+
+  def show_in_order(self):
+    """left, current, right"""
+    pass
+
+  def show_post_order(self):
+    """left, right, current"""
+    pass
+
+  def show_pre_order(self):
+    """current, left, right"""
+    pass
+
+  def balance(self):
+    pass
+
--- data_structures/test_binary_search_tree.py
+++ data_structures/test_binary_search_tree.py
@@ -1,40 +1,107 @@
-from binary_search_tree import BinaryTree
+from binary_search_tree import BinarySearchTree
 from random import randint, shuffle
 
-# small tree
-
-bt = BinaryTree()
-bt.add('a')
-bt.add('d')
-bt.add('e')
-bt.add('f')
-bt.add('b')
-bt.add('c')
-
-bt.show_all()
-bt.show_all_visual()
-
-for e in ['a','b','g']:
+def check_for(bt, e):
   if e in bt:
     print "%s was found" % e
   else:
     print "%s was not found" % e
 
-print
+def load(bt):
+  # a nicely balanced tree
+  bt.add(10)
+  bt.add(5)
+  bt.add(2)
+  bt.add(1)
+  bt.add(4)
+  bt.add(7)
+  bt.add(6)
+  bt.add(9)
+  bt.add(15)
+  bt.add(12)
+  bt.add(11)
+  bt.add(14)
+  bt.add(17)
+  bt.add(16)
+  bt.add(19)
+
+# small tree
+
+if False:
+
+  bt = BinarySearchTree()
+  bt.add('a')
+  bt.add('d')
+  bt.add('e')
+  bt.add('f')
+  bt.add('b')
+  bt.add('c')
+
+  bt.show_all()
+  bt.show_all_visual()
+
+  for e in ['a','e','j']:
+    check_for(bt,e)
+
+  print
+
+# deleting
+
+if False:
+
+  bt = BinarySearchTree()
+  load(bt)
+  bt.show_all_visual()
+
+  print "\ndeleting leafless node 19"
+  bt.rm(19)
+  check_for(bt,19)
+  bt.show_all_visual()
+
+  print "\nreloading and deleting middle node 15"
+  bt = BinarySearchTree()
+  load(bt)
+  bt.rm(15)
+  bt.show_all_visual()
+
+  print "\nreloading and deleting root node 10"
+  bt = BinarySearchTree()
+  load(bt)
+  bt.rm(10)
+  bt.show_all_visual()
 
 # big tree
 
-bt2 = BinaryTree()
+if False:
 
-# get ~100 distinct random numbers from 1 to 1000
-random_number_hash = {}
-for i in range(0,100):
-  random_number_hash[randint(0,1000)] = True
-random_numbers = [e for e in random_number_hash.keys()]
-shuffle(random_numbers)
+  bt2 = BinarySearchTree()
 
-for e in random_numbers:
-  bt2.add(e)
+  # get ~100 distinct random numbers from 1 to 1000
+  random_number_hash = {}
+  for i in range(0,100):
+    random_number_hash[randint(0,1000)] = True
+  random_numbers = [e for e in random_number_hash.keys()]
+  shuffle(random_numbers)
 
-bt2.show_all_visual()
+  for e in random_numbers:
+    bt2.add(e)
 
+  bt2.show_all_visual()
+
+# huge tree
+
+if False:
+
+  bt2 = BinarySearchTree()
+
+  # get ~10,000 distinct random numbers from 1 to 10,000,000
+  random_number_hash = {}
+  for i in range(0,10000):
+    random_number_hash[randint(0,10000000)] = True
+  random_numbers = [e for e in random_number_hash.keys()]
+  shuffle(random_numbers)
+
+  for e in random_numbers:
+    bt2.add(e)
+
+  # todo: look for one number
