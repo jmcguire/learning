1bc1e020da04cd1fd15cc425ea374e64cf6f1ce2
Justin McGuire <jm@landedstar.com>
2016-07-13 21:28:43 -0400

    misc algorithms, for fun stuff and coding examples
     - fib.py - fibonacci
     - count_bits.py - count # of turned on bits in a byte

---
+++ algorithms/misc/count_bits.py
@@ -0,0 +1,11 @@
+def count_bits(n):
+	"""count the number of bits set to 1 in n"""
+	count = 0
+	for i in range(0,32):
+		if n & (1<<i):
+			count += 1
+	return count
+
+for test in range(0,100):
+	print "test: %d, %s, %s" % (test, bin(test), count_bits(test))
+
